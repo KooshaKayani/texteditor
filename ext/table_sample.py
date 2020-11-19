21e4bd0cc7eaf0361c3b8a585e0e5d2cb722314d
Koosha.Kayani <koosha18@users.noreply.github.com>
2020-11-19 20:14:03 +1100

    The table is now disabled for update

--- ext/table_sample.py
+++ ext/table_sample.py
@@ -2,8 +2,8 @@ import sys
 
 #PYQT5 PyQt4’s QtGui module has been split into PyQt5’s QtGui, QtPrintSupport and QtWidgets modules
 
-from PyQt5 import QtWidgets 
-from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import * 
 #PYQT5 QSpinBox, QMessageBox, QDialog, QPushButton, QGridLayout, QLabel
 
 
@@ -16,92 +16,167 @@ class Table(QtWidgets.QDialog):
         QtWidgets.QDialog.__init__(self, parent)
 
         self.parent = parent
-         
-        self.initUI()
+  
+        self.initUI(Dialog=  QtWidgets.QDialog())
  
-    def initUI(self):
-
-        # Rows
-        rowsLabel = QtWidgets.QLabel("Rows: ",self)
+    def initUI(self, Dialog ):
         
-        self.rows = QtWidgets.QSpinBox(self)
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(350, 350)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
+        Dialog.setMinimumSize(QtCore.QSize(350, 350))
+        Dialog.setMaximumSize(QtCore.QSize(350, 350))
+        Dialog.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 0);")
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 10)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 0);")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout.setContentsMargins(-1, 10, -1, 0)
+        self.gridLayout.setVerticalSpacing(10)
+        self.gridLayout.setObjectName("gridLayout")
+        self.rowlable = QtWidgets.QLabel(self.frame)
+        self.rowlable.setObjectName("rowlable")
+        self.gridLayout.addWidget(self.rowlable, 0, 0, 1, 1)
+        self.Rows = QtWidgets.QSpinBox(self.frame)
+        self.Rows.setMinimumSize(QtCore.QSize(100, 25))
+        self.Rows.setStyleSheet("border:1px solid;\n"
+"border-color: rgb(0, 0, 0);")
+        self.Rows.setObjectName("Rows")
+        self.gridLayout.addWidget(self.Rows, 0, 1, 1, 1)
+        self.columnlable = QtWidgets.QLabel(self.frame)
+        self.columnlable.setObjectName("columnlable")
+        self.gridLayout.addWidget(self.columnlable, 1, 0, 1, 1)
+        self.Column = QtWidgets.QSpinBox(self.frame)
+        self.Column.setMinimumSize(QtCore.QSize(100, 25))
+        self.Column.setStyleSheet("border:1px solid;\n"
+"border-color: rgb(0, 0, 0);")
+        self.Column.setObjectName("Column")
+        self.gridLayout.addWidget(self.Column, 1, 1, 1, 1)
+        self.paddinglable = QtWidgets.QLabel(self.frame)
+        self.paddinglable.setObjectName("paddinglable")
+        self.gridLayout.addWidget(self.paddinglable, 2, 0, 1, 1)
+        self.padding = QtWidgets.QSpinBox(self.frame)
+        self.padding.setMinimumSize(QtCore.QSize(100, 25))
+        self.padding.setStyleSheet("border:1px solid;\n"
+"border-color: rgb(0, 0, 0);")
+        self.padding.setObjectName("padding")
+        self.gridLayout.addWidget(self.padding, 2, 1, 1, 1)
+        self.spacinglable = QtWidgets.QLabel(self.frame)
+        self.spacinglable.setObjectName("spacinglable")
+        self.gridLayout.addWidget(self.spacinglable, 3, 0, 1, 1)
+        self.spacing = QtWidgets.QSpinBox(self.frame)
+        self.spacing.setMinimumSize(QtCore.QSize(100, 25))
+        self.spacing.setStyleSheet("border:1px solid;\n"
+"border-color: rgb(0, 0, 0);")
+        self.spacing.setObjectName("spacing")
+        self.gridLayout.addWidget(self.spacing, 3, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
+        self.color = QtWidgets.QPushButton(self.frame)
+        self.color.setMinimumSize(QtCore.QSize(0, 25))
+        self.color.setStyleSheet("border: 1px solid;")
+        self.color.setText("")
+        self.color.setObjectName("color")
+        self.color.clicked.connect(self.colorD)
+        self.gridLayout.addWidget(self.color, 4, 1, 1, 1)
+        self.verticalLayout.addWidget(self.frame)
+        self.frame_2 = QtWidgets.QFrame(Dialog)
+        self.frame_2.setMinimumSize(QtCore.QSize(0, 40))
+        self.frame_2.setMaximumSize(QtCore.QSize(16777215, 40))
+        self.frame_2.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 0);")
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.ok = QtWidgets.QPushButton(self.frame_2)
+        self.ok.setMinimumSize(QtCore.QSize(80, 25))
+        self.ok.setMaximumSize(QtCore.QSize(60, 20))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        self.ok.setFont(font)
+        self.ok.setStyleSheet("border : 1px solid;\n"
+"color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);")
+        self.ok.setObjectName("ok")
+        self.verticalLayout_2.addWidget(self.ok)
+        self.ok.clicked.connect(self.insert)
+        self.verticalLayout.addWidget(self.frame_2, 0, QtCore.Qt.AlignHCenter)
 
-        # Columns
-        colsLabel = QtWidgets.QLabel("Columns",self)
         
-        self.cols = QtWidgets.QSpinBox(self)
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Table"))
+        self.rowlable.setText(_translate("Dialog", "Rows:"))
+        self.columnlable.setText(_translate("Dialog", "Column:"))
+        self.paddinglable.setText(_translate("Dialog", "Padding:"))
+        self.spacinglable.setText(_translate("Dialog", "Spacing:"))
+        self.label.setText(_translate("Dialog", "Color:"))
+        self.ok.setText(_translate("Dialog", "ok"))
 
-        # Cell spacing (distance between cells)
-        spaceLabel = QtWidgets.QLabel("Cell spacing",self)
-        
-        self.space = QtWidgets.QSpinBox(self)
-
-        # Cell padding (distance between cell and inner text)
-        padLabel = QtWidgets.QLabel("Cell padding",self)
-
-        self.pad = QtWidgets.QSpinBox(self)
-        
-        self.pad.setValue(10)
-
-        # Button
-        insertButton = QtWidgets.QPushButton("Insert",self)
-        insertButton.clicked.connect(self.insert)
-
-        # Layout
-        layout = QtWidgets.QGridLayout()
-
-        layout.addWidget(rowsLabel,0,0)
-        layout.addWidget(self.rows,0,1)
-
-        layout.addWidget(colsLabel,1,0)
-        layout.addWidget(self.cols,1,1)
-
-        layout.addWidget(padLabel,2,0)
-        layout.addWidget(self.pad,2,1)
-        
-        layout.addWidget(spaceLabel,3,0)
-        layout.addWidget(self.space,3,1)
-
-        layout.addWidget(insertButton,4,0,1,2)
-
-        self.setWindowTitle("Insert Table")
-        self.setGeometry(300,300,200,100)
-        self.setLayout(layout)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+    
+    def colorD(self):
+        Tcolor = QColorDialog.getColor()
+        self.color.setStyleSheet("border: 1px solid;\n background-color: " + Tcolor.name() + ';' )
 
     def insert(self):
 
-        cursor = self.parent.ui.textEdit.textCursor()
+            cursor = self.parent.textEdit.textCursor()
 
-        # Get the configurations
-        rows = self.rows.value()
+            # Get the configurations
+            rows = self.Rows.value()
 
-        cols = self.cols.value()
+            cols = self.Column.value()
 
-        if not rows or not cols:
+            if not rows or not cols:
 
-            popup = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Warning,
-                                      "Parameter error",
-                                      "Row and column numbers may not be zero!",
-                                      QtWidgets.QMessageBox.Ok,
-                                      self)
-            popup.show()
+                popup = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Warning,
+                                        "Parameter error",
+                                        "Row and column numbers may not be zero!",
+                                        QtWidgets.QMessageBox.Ok,
+                                        self)
+                popup.show()
 
-        else:
+            else:
 
-            padding = self.pad.value()
+                padding = self.padding.value()
 
-            space = self.space.value()
+                space = self.spacing.value()
 
-            # Set the padding and spacing
-            fmt = QtGui.QTextTableFormat()
+                # Set the padding and spacing
+                fmt = QtGui.QTextTableFormat()
+                
+                fmt.setCellPadding(padding)
 
-            fmt.setBackground(QColor("#00FFFF"))      
+                fmt.setCellSpacing(space)
 
-            fmt.setCellPadding(padding)
+                # Inser the new table
+                cursor.insertTable(rows,cols,fmt)
 
-            fmt.setCellSpacing(space)
+                self.close()
 
-            # Inser the new table
-            cursor.insertTable(rows,cols,fmt)
-
-            self.close()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Table()
+    ui.initUI(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import *
 # GUI FILE
 from ui_main import Ui_MainWindow
 #from table_sample import Ui_Table
-from ext import *
+from ext import table_sample
 FONT_SIZES = [7, 8, 9, 10, 11, 12, 13, 14, 18, 24, 36, 48, 64, 72, 96, 144, 288]
 
 class MainWindow(QMainWindow):
@@ -53,6 +53,11 @@ class MainWindow(QMainWindow):
         self.ui.actionTEXT.triggered.connect(self.HTtoText)
         self.ui.textEdit.textChanged.connect(self.HTML)
         #self.ui.htmlEdit.textChanged.connect(self.HTtoText)
+        
+        # We need our own context menu for tables
+        self.ui.textEdit.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.ui.textEdit.customContextMenuRequested.connect(self.context)
+
 
     # all the functions for the tool bar
     def HTML(self):
@@ -109,7 +114,169 @@ class MainWindow(QMainWindow):
         # If there is no selection, just insert a tab
         else:
             cursor.insertText("\t")
-            
+
+    def context(self,pos):
+
+        # Grab the cursor
+        cursor = self.ui.textEdit.textCursor()
+
+        # Grab the current table, if there is one
+        table = cursor.currentTable()
+
+        # Above will return 0 if there is no current table, in which case
+        # we call the normal context menu. If there is a table, we create
+        # our own context menu specific to table interaction
+        if table:
+
+            menu = QtWidgets.QMenu(self)
+
+            appendRowAction = QtWidgets.QAction("Append row",self)
+            appendRowAction.triggered.connect(lambda: table.appendRows(1))
+
+            appendColAction = QtWidgets.QAction("Append column",self)
+            appendColAction.triggered.connect(lambda: table.appendColumns(1))
+
+
+            removeRowAction = QtWidgets.QAction("Remove row",self)
+            removeRowAction.triggered.connect(self.removeRow)
+
+            removeColAction = QtWidgets.QAction("Remove column",self)
+            removeColAction.triggered.connect(self.removeCol)
+
+
+            insertRowAction = QtWidgets.QAction("Insert row",self)
+            insertRowAction.triggered.connect(self.insertRow)
+
+            insertColAction = QtWidgets.QAction("Insert column",self)
+            insertColAction.triggered.connect(self.insertCol)
+
+
+            mergeAction = QtWidgets.QAction("Merge cells",self)
+            mergeAction.triggered.connect(lambda: table.mergeCells(cursor))
+
+            # Only allow merging if there is a selection
+            if not cursor.hasSelection():
+                mergeAction.setEnabled(False)
+
+
+            splitAction = QtWidgets.QAction("Split cells",self)
+
+            cell = table.cellAt(cursor)
+
+            # Only allow splitting if the current cell is larger
+            # than a normal cell
+            if cell.rowSpan() > 1 or cell.columnSpan() > 1:
+
+                splitAction.triggered.connect(lambda: table.splitCell(cell.row(),cell.column(),1,1))
+
+            else:
+                splitAction.setEnabled(False)
+
+
+            menu.addAction(appendRowAction)
+            menu.addAction(appendColAction)
+
+            menu.addSeparator()
+
+            menu.addAction(removeRowAction)
+            menu.addAction(removeColAction)
+
+            menu.addSeparator()
+
+            menu.addAction(insertRowAction)
+            menu.addAction(insertColAction)
+
+            menu.addSeparator()
+
+            menu.addAction(mergeAction)
+            menu.addAction(splitAction)
+
+            # Convert the widget coordinates into global coordinates
+            pos = self.mapToGlobal(pos)
+
+            # Add pixels for the tool and formatbars, which are not included
+            # in mapToGlobal(), but only if the two are currently visible and
+            # not toggled by the user
+
+            '''if self.toolbar.isVisible():
+                pos.setY(pos.y() + 45)
+
+            if self.formatbar.isVisible():
+                pos.setY(pos.y() + 45)'''
+                
+            # Move the menu to the new position
+            menu.move(pos)
+
+            menu.show()
+
+        else:
+
+            event = QtGui.QContextMenuEvent(QtGui.QContextMenuEvent.Mouse,QtCore.QPoint())
+
+            self.ui.textEdit.contextMenuEvent(event)
+
+    def removeRow(self):
+
+        # Grab the cursor
+        cursor = self.ui.textEdit.textCursor()
+
+        # Grab the current table (we assume there is one, since
+        # this is checked before calling)
+        table = cursor.currentTable()
+
+        # Get the current cell
+        cell = table.cellAt(cursor)
+
+        # Delete the cell's row
+        table.removeRows(cell.row(),1)
+
+    def removeCol(self):
+
+        # Grab the cursor
+        cursor = self.ui.textEdit.textCursor()
+
+        # Grab the current table (we assume there is one, since
+        # this is checked before calling)
+        table = cursor.currentTable()
+
+        # Get the current cell
+        cell = table.cellAt(cursor)
+
+        # Delete the cell's column
+        table.removeColumns(cell.column(),1)
+
+    def insertRow(self):
+
+        # Grab the cursor
+        cursor = self.ui.textEdit.textCursor()
+
+
+        # Grab the current table (we assume there is one, since
+        # this is checked before calling)
+        table = cursor.currentTable()
+
+        # Get the current cell
+        cell = table.cellAt(cursor)
+
+        # Insert a new row at the cell's position
+        table.insertRows(cell.row(),1)
+
+    def insertCol(self):
+
+        # Grab the cursor
+        cursor = self.ui.textEdit.textCursor()
+
+
+        # Grab the current table (we assume there is one, since
+        # this is checked before calling)
+        table = cursor.currentTable()
+
+        # Get the current cell
+        cell = table.cellAt(cursor)
+
+        # Insert a new row at the cell's position
+        table.insertColumns(cell.column(),1)
+
     def insertImage(self):
 
         # Get image file name
